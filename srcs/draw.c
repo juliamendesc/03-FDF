b52c7a7f6d954af5389d1e9376c4c7eda24e7b91
Julia Mendes <juliamendesc@gmail.com>
2022-02-21 21:07:39 +0000

    adapt functions to t_point

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@ void isometric_projection(float *x, float *y, int z)
 	*y = (*x + *y) * sin(0.8) + z;
 }
 
-void draw_line(float x, float y, float x1, float y1, t_coordinates *coord, t_program *img)
+void draw_line(t_point m0, t_point m1, t_coordinates *coord, t_program *img)
 {
 	float x_step;
 	float y_step;
@@ -14,33 +14,31 @@ void draw_line(float x, float y, float x1, float y1, t_coordinates *coord, t_pro
 	int z;
 	int z1;
 
-	z = coord->z_matrix[(int)y][(int)x];
-	z1 = coord->z_matrix[(int)y1][(int)x1];
+	m0.z = coord->z_matrix[(int)m0.y][(int)m1.x];
+	m1.z = coord->z_matrix[(int)m1.y][(int)m1.x];
 
-	x *= coord->zoom;
-	y *= coord->zoom;
-	x1 *= coord->zoom;
-	y1 *= coord->zoom;
+	m0.x *= coord->zoom;
+	m0.y *= coord->zoom;
+	m1.x *= coord->zoom;
+	m1.y *= coord->zoom;
 
-	if (z != 0)
+	if (m0.z != 0)
 		coord->color = 0xE80C0C;
 	else
 		coord->color = 0xFFFFFFFF;
-	x_step = x1 - x;
-	y_step = y1 - y;
+	x_step = m1.x - m0.x;
+	y_step = m1.y - m0.y;
 	// isometric_projection(&x, &y, z);
 	// isometric_projection(&x1, &y1, z1);
 	max = MAX(ft_abs_float(x_step), ft_abs_float(y_step));
 	x_step /= max;
 	y_step /= max;
-	while ((int)(x - x1) || (int)(y - y1))
+	while ((int)(m0.x - m1.x) || (int)(m0.y - m1.y))
 	{
-		my_mlx_pixel_put(img, x, y, coord->color);
-		x += x_step;
-		y += y_step;
-		printf("INSIDE LOOP x=%f, y=%f, x1=%f, y1=%f\n", x, y, x1, y1);
+		my_mlx_pixel_put(img, m0.x, m0.y, coord->color);
+		m0.x += x_step;
+		m0.y += y_step;
 	}
-	printf("AFTER LINE DRAW x=%f, y=%f, x1=%f, y1=%f\n", x, y, x1, y1);
 }
 
 void draw_map(t_coordinates *coord, t_program *img)
@@ -58,13 +56,13 @@ void draw_map(t_coordinates *coord, t_program *img)
 			{
 				m1.x = m0.x + 1;
 				m1.y = m0.y;
-				draw_line(m0.x, m0.y, m1.x, m1.y, coord, img);
+				draw_line(m0, m1, coord, img);
 			}
 			if (m0.y < coord->height - 1)
 			{
 				m1.x = m0.x;
 				m1.y = m0.y + 1;
-				draw_line(m0.x, m0.y, m1.x, m1.y, coord, img);
+				draw_line(m0, m1, coord, img);
 			}
 			m0.x++;
 		}
