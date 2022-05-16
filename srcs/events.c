0aaa6268b94e52e14996c57844fcff369ddf975d
Julia Mendes <juliamendesc@gmail.com>
2022-05-16 11:52:44 +0100

    implement all bonuses

--- srcs/events.c
+++ srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: julcarva <julcarva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 20:41:42 by julcarva          #+#    #+#             */
-/*   Updated: 2022/05/16 00:33:03 by julcarva         ###   ########.fr       */
+/*   Updated: 2022/05/16 11:52:23 by julcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,21 @@ int key_press(int keycode, void *param)
 	t_program *fdf;
 
 	fdf = (t_program *)param;
-	printf("keycode: %d\n", keycode);
 	if (keycode == 53 || keycode == 65307)
 		exit(0);
 	else if (keycode == 69 || keycode == 43 || keycode == 78 || keycode == 45)
 		zoom(keycode, fdf);
-	else if (keycode == 109 || keycode == 110)
+	else if (keycode == 113 || keycode == 119)
 		flatten_z(keycode, fdf);
 	else if (keycode == 65362 || keycode == 126 || keycode == 65361 || keycode == 125 || keycode == 65363 || keycode == 123 || keycode == 65364 || keycode == 124)
 		move(keycode, fdf);
-	else if (keycode == 113 || keycode == 97 || keycode == 119 || keycode == 115 || keycode == 101 || keycode == 100)
+	else if (keycode == 97 || keycode == 115)
 		rotate(keycode ,fdf);
-	// else if (keycode == 36 || keycode == 65293)
-	// 	fdf->zoom = 20;
-	// fdf->angle = 0.8;
+	else if (keycode == 122 || keycode == 120)
+		change_projection(keycode, fdf);
+	else if (keycode == 36 || keycode == 65293)
+		reset_view(keycode, &fdf);
 	return (0);
-}
-
-int leave_window(void)
-{
-	exit(0);
 }
 
 void zoom(int keycode, t_program *fdf)
@@ -50,41 +45,33 @@ void zoom(int keycode, t_program *fdf)
 	draw_map(fdf);
 }
 
-void move(int keycode, t_program *data)
+void move(int keycode, t_program *fdf)
 {
 	if (keycode == 65362 || keycode == 126)
-		data->adapt_y += 10;
+		fdf->adapt_y += 10;
 	else if (keycode == 65361 || keycode == 125)
-		data->adapt_x += 10;
+		fdf->adapt_x += 10;
 	else if (keycode == 65364 || keycode == 123)
-		data->adapt_y -= 10;
+		fdf->adapt_y -= 10;
 	else if (keycode == 65363 || keycode == 124)
-		data->adapt_x -= 10;
-	draw_map(data);
+		fdf->adapt_x -= 10;
+	draw_map(fdf);
 }
 
-void flatten_z(int keycode, t_program *data)
+void flatten_z(int keycode, t_program *fdf)
 {
-	if (keycode == 109)
-		data->zoom_z++;
-	else if (keycode == 110)
-		data->zoom_z--;
-	draw_map(data);
+	if (keycode == 113) //q
+		fdf->zoom_z++;
+	else if (keycode == 119) //w
+		fdf->zoom_z--;
+	draw_map(fdf);
 }
 
 void	rotate(int keycode, t_program *fdf)
 {
-	if (keycode == 113)
-		fdf->alpha += 0.05;
-	else if (keycode == 97)
-		fdf->alpha -= 0.05;
-	else if (keycode == 119)
-		fdf->beta -= 0.05;
-	else if (keycode == 115 )
-		fdf->beta += 0.05;
-	else if (keycode == 101)
-		fdf->gamma += 0.05;
-	else if (keycode == 100 )
-		fdf->gamma -= 0.05;
+	if (keycode == 115) //s
+		fdf->angle += 0.05;
+	else if (keycode == 97) //a
+		fdf->angle -= 0.05;
 	draw_map(fdf);
 }
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: julcarva <julcarva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 20:42:43 by julcarva          #+#    #+#             */
-/*   Updated: 2022/05/16 00:40:39 by julcarva         ###   ########.fr       */
+/*   Updated: 2022/05/16 11:49:13 by julcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,23 @@ void	my_mlx_pixel_put(t_program *data, int x, int y, int color)
 				+ x * (data->bits_per_pixel / 8));
 		*(unsigned int *)pixel = color;
 	}
+}
+
+int	leave_window(void)
+{
+	exit(0);
+}
+
+void	reset_view(int keycode, t_program **fdf)
+{
+	if (keycode == 36 || keycode == 65293)
+	{
+		(*fdf)->adapt_x = WIDTH / 3;
+		(*fdf)->adapt_y = HEIGHT / 3;
+		(*fdf)->angle = 0.523599;
+		(*fdf)->zoom = (WIDTH / HEIGHT) * 10;
+		(*fdf)->zoom_z = 5;
+		(*fdf)->is_isometric = 1;
+	}
+	draw_map(*fdf);
 }
